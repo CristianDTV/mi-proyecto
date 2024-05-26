a829ab3d1830a463aecd46c96af4f245ba02d30c
CristianDTV <cristian.travez@epn.edu.ec>
2024-05-26 13:12:54 -0500

    Version W 2.0 se agrego Readme

---
+++ README.md
@@ -0,0 +1,66 @@
+# Traductor de Braille
+
+## Introducción
+
+El Traductor de Braille es una aplicación diseñada para convertir texto alfabético a braille y generar archivos PDF con el texto traducido. La aplicación permite a los usuarios traducir texto y guardar la traducción en un archivo PDF.
+
+## Requisitos del Sistema
+
+- Python 3.11 o superior
+- Paquetes necesarios:
+  - customtkinter
+  - reportlab
+
+## Instalación
+
+1. **Instalar Python**: Asegúrese de tener Python 3.11 o superior instalado en su sistema. Puede descargar Python desde [python.org](https://www.python.org/).
+
+2. **Instalar Dependencias**: Ejecute el siguiente comando para instalar las dependencias necesarias:
+
+   ```bash
+   pip install customtkinter reportlab
+
+3. **Descargar la Aplicación**: Descargue el código fuente de la aplicación desde el repositorio o desde el archivo ZIP proporcionado.
+
+4. **Ubicar Archivos**:Asegúrese de que los archivos main.py, braille_conversion.py, pdf_generation.py y user_data.json estén en el mismo directorio.
+
+## Iniciar la Aplicación
+
+Para iniciar la aplicación, abra una terminal, navegue al directorio donde se encuentra main.py y ejecute:
+
+python main.py
+
+## Uso de la Aplicación
+
+Traducción de Texto a Braille
+
+1. **Ingresar Texto**: En la ventana principal, introduzca el texto que desea convertir en el campo "Texto a Convertir".
+2. **Traducción Automática**: La traducción a braille se realizará automáticamente y aparecerá en el campo "Texto en Braille".
+
+## Guardar Traducción como PDF
+
+1. **Hacer Clic en "Guardar como PDF Espejo"**: En la ventana principal, haga clic en el botón para guardar la traducción.
+2. **Seleccionar Ubicación**: Elija la ubicación donde desea guardar el archivo PDF.
+3. **Confirmación**: Aparecerá un mensaje confirmando que el archivo PDF se ha creado correctamente.
+
+## Acerca De
+
+1. **Hacer Clic en "Acerca De"**: En el menú de la izquierda, haga clic en el botón "Acerca De".
+2. **Información**: Aparecerá una ventana emergente con información sobre la aplicación.
+
+## Resolución de Problemas
+
+1. **Problema**: Error al guardar el archivo PDF
+Solución: Asegúrese de que la ruta seleccionada sea válida y que tenga permisos para escribir en esa ubicación.
+2. **Problema**: La traducción a braille no aparece
+Solución: Asegúrese de que el campo de texto de entrada no esté vacío y que el formato del texto sea correcto.
+3. **Problema**: La aplicación no se abre
+Solución: Verifique que Python esté instalado correctamente y que todas las dependencias necesarias estén instaladas. Ejecute la aplicación desde la terminal para ver cualquier mensaje de error.
+
+## Agradecimientos
+
+Queremos agradecer a todos los desarrolladores y probadores que han contribuido a esta versión. Su dedicación y esfuerzo han sido cruciales para el desarrollo de esta aplicación.
+
+## Contacto
+
+ Para más información, por favor contacte a CristianDTV.
--- src/main.py
+++ src/main.py
@@ -4,17 +4,17 @@ from tkinter import filedialog, messagebox
 from braille_conversion import texto_a_braille
 from pdf_generation import generar_pdf_espejo
 
-ctk.set_appearance_mode("System")  
-ctk.set_default_color_theme("blue")  
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
 
 def convertir_a_braille(event=None):
-    texto = text_input.get("1.0", "end-1c").strip()  
+    texto = text_input.get("1.0", "end-1c").strip()
     texto_braille = texto_a_braille(texto)
     text_output.delete("1.0", "end")
     text_output.insert("1.0", texto_braille)
 
 def guardar_como_pdf_espejo():
-    texto_braille = text_output.get("1.0", "end-1c").strip()  
+    texto_braille = text_output.get("1.0", "end-1c").strip()
     if texto_braille:
         file_path = filedialog.asksaveasfilename(defaultextension=".pdf", filetypes=[("PDF files", "*.pdf")])
         if file_path:
@@ -22,7 +22,7 @@ def guardar_como_pdf_espejo():
             messagebox.showinfo("Éxito", "Se ha creado el PDF correctamente.")
 
 def mostrar_acerca_de():
-    messagebox.showinfo("Acerca De", "Traductor de Braille\nVersión W\nDesarrollado por: KDR CONSULTECH \nEPN 2024")
+    messagebox.showinfo("Acerca De", "Traductor de Braille\nVersión Z\nDesarrollado por: CristianDTV\nEPN 2024")
 
 def centrar_ventana(ventana, ancho, alto):
     pantalla_ancho = ventana.winfo_screenwidth()
@@ -34,11 +34,10 @@ def centrar_ventana(ventana, ancho, alto):
 def crear_gui():
     window = ctk.CTk()
     window.title("Traductor de Braille")
-    window.geometry("1000x800")  
+    window.geometry("1000x800")
     centrar_ventana(window, 900, 700)
-    window.resizable(True, True)  
+    window.resizable(True, True)
 
-    
     frame_menu = ctk.CTkFrame(window, width=200, height=800, corner_radius=0)
     frame_menu.pack(side="left", fill="y")
 
@@ -48,7 +47,6 @@ def crear_gui():
     btn_acerca_de = ctk.CTkButton(frame_menu, text="Acerca De", command=mostrar_acerca_de)
     btn_acerca_de.pack(fill="x", padx=20, pady=10)
 
-    
     frame_main = ctk.CTkFrame(window, width=800, height=800, corner_radius=0)
     frame_main.pack(side="right", fill="both", expand=True)
 
@@ -57,14 +55,14 @@ def crear_gui():
     label_input = ctk.CTkLabel(frame_main, text="Texto a Convertir", font=("Helvetica", 16))
     label_input.pack(pady=10)
 
-    text_input = ctk.CTkTextbox(frame_main, height=200, width=500, font=("Helvetica", 16))  
+    text_input = ctk.CTkTextbox(frame_main, height=200, width=500, font=("Helvetica", 16))
     text_input.pack(pady=10)
     text_input.bind("<KeyRelease>", convertir_a_braille)
 
     label_output = ctk.CTkLabel(frame_main, text="Texto en Braille", font=("Helvetica", 16))
     label_output.pack(pady=10)
 
-    text_output = ctk.CTkTextbox(frame_main, height=200, width=500, font=("Helvetica", 16))  
+    text_output = ctk.CTkTextbox(frame_main, height=200, width=500, font=("Helvetica", 16))
     text_output.pack(pady=10)
 
     btn_guardar_pdf_espejo = ctk.CTkButton(frame_main, text="Guardar como PDF Espejo", command=guardar_como_pdf_espejo)
